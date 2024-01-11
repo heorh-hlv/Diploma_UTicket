7ff2045eb0fd59a98227c5ed31f85d97491f2d7d
heorh-hlv <138703607+heorh-hlv@users.noreply.github.com>
2024-01-11 18:35:53 +0200

    created base, reg/log not working

--- Code/Diploma_UTicket/UTicket/urls.py
+++ Code/Diploma_UTicket/UTicket/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home_page, name="index"),
-    path("home", views.home_page, name="sindex"),
-    path("booking", views.booking_page, name="booking"),
-    path("check_booking", views.check_page, name="check_booking"),
-    path("cancel_booking", views.cancel_page, name="cancel_booking"),
+    path("home", views.home_page, name="home"),
+    path("booking/", views.booking_page, name="booking"),
+    path("check_booking/", views.check_page, name="check_booking"),
+    path("cancel_booking/", views.cancel_page, name="cancel_booking"),
 ]
--- Code/Diploma_UTicket/UTicket/views.py
+++ Code/Diploma_UTicket/UTicket/views.py
@@ -6,51 +6,48 @@ from django.contrib.auth import logout
 # Create your views here.
 def home_page(request):
     return render(request, 'index.html')
-#@login_required
+@login_required
 def booking_page(request):
     return render(request, 'booking.html')
-#@login_required
+@login_required
 def cancel_page(request):
     return render(request, 'cancel_booking.html')
-#@login_required
+@login_required
 def check_page(request):
     return render(request, 'check_booking.html')
 
-# def register(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         first_name = request.POST['first_name']
-#         last_name = request.POST['last_name']
-#         email = request.POST["email"]
-#         password1, password2 = request.POST["password1"], request.POST["password2"]
-#         if username is None:
-#             return HttpResponse("<h3>Введите имя пользователя</h3>")
-#         elif email is None:
-#             return HttpResponse("<h3>Введите почту</h3>")
-#         elif first_name is None:
-#             return HttpResponse("<h3>Введите полное 1имя</h3>")
-#         elif last_name is None:
-#             return HttpResponse("<h3>Введите полное 2имя</h3>")
-#         elif password1 is None or password2 is None:
-#             return HttpResponse("<h3>Введите пароль</h3>")
-#         elif password1 != password2:
-#             return HttpResponse("<h3>Пароли должны совпадать</h3>")
-#         else:
-#             user = NewUser.objects.create_user(username=username, password=password1, first_name=first_name, last_name=last_name)
-#             login(request, user)
-#             return redirect('home')
-#     return render(request, 'reg.html')
+def register(request):
+    if "register" in request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST["email"]
+        password1, password2 = request.POST["password1"], request.POST["password2"]
+        if username is None:
+            return HttpResponse("<h3>Введите имя пользователя</h3>")
+        elif email is None:
+            return HttpResponse("<h3>Введите почту</h3>")
+        elif first_name is None:
+            return HttpResponse("<h3>Введите полное 1имя</h3>")
+        elif last_name is None:
+            return HttpResponse("<h3>Введите полное 2имя</h3>")
+        elif password1 is None or password2 is None:
+            return HttpResponse("<h3>Введите пароль</h3>")
+        elif password1 != password2:
+            return HttpResponse("<h3>Пароли должны совпадать</h3>")
+        else:
+            user = NewUser.objects.create_user(username=username, password=password1, first_name=first_name, last_name=last_name)
+            login(request, user)
+            return redirect('login')
+    if "login" in request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('login')
+    return render(request, 'index.html')
 
-# def login_view(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = authenticate(username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return redirect('home')
-#     return render(request, 'index.html')
-#
 # @login_required
 # def logout_view(request):
 #     logout(request)
