537f2f9a05ec7d32277f997d3c21739b878e4e21
heorh-hlv <138703607+heorh-hlv@users.noreply.github.com>
2024-01-26 18:28:35 +0200

    Auth 1

--- Code/Diploma_UTicket/UTicket/models.py
+++ Code/Diploma_UTicket/UTicket/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class NewUser(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True, )
+    date_of_birth = models.DateField(null=True, blank=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
 
 
--- Code/Diploma_UTicket/UTicket/views.py
+++ Code/Diploma_UTicket/UTicket/views.py
@@ -6,21 +6,21 @@ from django.contrib.auth import logout
 # Create your views here.
 def home_page(request):
     return render(request, 'index.html')
-# @login_required
+@login_required
 def booking_page(request):
     return render(request, 'booking.html')
-# @login_required
+@login_required
 def cancel_page(request):
     return render(request, 'cancel_booking.html')
-# @login_required   
+@login_required
 def check_page(request):
     return render(request, 'check_booking.html')
     
 def login(request):
     if  request.method == "POST":
-        username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
-        user = authenticate(username=username, password=password)
+        user = authenticate(email=email, password=password)
         if user is not None:
             login(request, user)
             return render(request, 'index')
@@ -28,25 +28,23 @@ def login(request):
 
 def signup(request):
     if request.method == "POST":
-        username = request.POST['username']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST["email"]
-        password1, password2 = request.POST["password1"], request.POST["password2"]
-        if username is None:
-            return HttpResponse("<h3>Введите имя пользователя</h3>")
-        elif email is None:
-            return HttpResponse("<h3>Введите почту</h3>")
-        elif first_name is None:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        date_of_birth = request.POST.get("date-of-birth")
+        if first_name is None:
             return HttpResponse("<h3>Введите полное 1имя</h3>")
         elif last_name is None:
             return HttpResponse("<h3>Введите полное 2имя</h3>")
-        elif password1 is None or password2 is None:
+        elif email is None:
+            return HttpResponse("<h3>Введите почту</h3>")
+        elif password is None:
             return HttpResponse("<h3>Введите пароль</h3>")
-        elif password1 != password2:
-            return HttpResponse("<h3>Пароли должны совпадать</h3>")
+        elif date_of_birth is None:
+            return HttpResponse("<h3>Введите дату рождения</h3>")
         else:
-            user = NewUser.objects.create_user(username=username, password=password1, first_name=first_name, last_name=last_name)
+            user = NewUser.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password, )
             login(request, user)
             return redirect('index')
     return render(request, 'signup.html')
