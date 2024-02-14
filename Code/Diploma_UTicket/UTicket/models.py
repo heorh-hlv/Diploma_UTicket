cf1dd0eeba8dba34321ce6ba8eec47013a8ca2f3
heorh-hlv <138703607+heorh-hlv@users.noreply.github.com>
2024-02-14 18:38:09 +0200

    New feature for booking, translated text

--- Code/Diploma_UTicket/UTicket/models.py
+++ Code/Diploma_UTicket/UTicket/models.py
@@ -3,15 +3,26 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
+
+
 # Create your models here.
 
 class NewUser(AbstractUser):
+
+    # Remove username field
     username = None
 
+    # Add email field as unique identifiers
+    # for authentication instead of usernames
     email = models.EmailField(_("email address"), unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
 
+    # Add d_o_b field
+    date_of_birth = models.DateField()
+
+    # Assign an instance of a custom manager
     objects = CustomUserManager()
+
+    # Declare fields
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
@@ -19,5 +30,25 @@ class NewUser(AbstractUser):
         return self.email
 
 
+class Tickets(models.Model):
 
+    # Travel details
+    city_of_departure = models.TextField()
+    city_destination = models.TextField()
+    departure_date = models.DateField()
+    return_date = models.DateField()
+    amount_of_passengers = models.IntegerField()
 
+    # Travel settings
+    plane_class = models.TextField()
+    flight_departure = models.TextField()
+    plane_place = models.TextField()
+
+    # Passenger data
+    first_name = models.TextField()
+    second_name = models.TextField()
+    date_of_birth = models.TextField()
+    phone_number = models.TextField()
+
+    # Connect tables
+    email = models.ForeignKey(NewUser, on_delete=models.CASCADE)
--- Code/Diploma_UTicket/UTicket/views.py
+++ Code/Diploma_UTicket/UTicket/views.py
@@ -1,15 +1,47 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .models import NewUser
+from .models import NewUser, Tickets
 from django.contrib import messages
 from django.contrib.auth import logout
 # Create your views here.
+
+
 def home_page(request):
     return render(request, 'index.html')
 
 @login_required
 def booking_page(request):
+    # if request.method == "POST":
+    #     city_of_departure = request.POST.get("city_of_departure")
+    #     city_destination = request.POST.get("city_destination")
+    #     departure_date = request.POST.get("departure_date")
+    #     return_date = request.POST.get("return_date")
+    #     amount_of_passengers = request.POST.get("amount_of_passengers")
+    #
+    #     # Travel settings
+    #     plane_class = request.POST.get("plane_class")
+    #     flight_departure = request.POST.get("flight_departure")
+    #     plane_place = request.POST.get("plane_place")
+    #
+    #     # Passenger data
+    #     first_name = request.POST.get("first_name")
+    #     second_name = request.POST.get("second_name")
+    #     date_of_birth = request.POST.get("date_of_birth")
+    #     phone_number = request.POST.get("phone_number")
+    #
+    #     # Connect tables
+    #     email = request.POST.get("email")
+    #
+    #     Tickets.objects.create(city_of_departure=city_of_departure, city_destination=city_destination,
+    #                                   departure_date=departure_date, return_date=return_date,
+    #                                   amount_of_passengers=amount_of_passengers, plane_class=plane_class,
+    #                                   flight_departure=flight_departure, plane_place=plane_place,
+    #                                   first_name=first_name, second_name=second_name, date_of_birth=date_of_birth,
+    #                                   phone_number=phone_number, email=email)
+    #
+    #
+    #     return redirect('index')
     return render(request, 'booking.html')
 
 @login_required
@@ -35,26 +67,41 @@ def login_view(request):
 
 
 def signup(request):
+
+    # Check if the request method is POST
     if request.method == "POST":
+
+        # Retrieve user input from the form
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         email = request.POST.get("email")
         password = request.POST.get("password")
         date_of_birth = request.POST.get("date-of-birth")
+
+        # Validate input fields
         if first_name is None:
-            return HttpResponse("<h3>Введите полное 1имя</h3>")
+            return HttpResponse("<h3>Введіть ім'я</h3>")
         elif last_name is None:
-            return HttpResponse("<h3>Введите полное 2имя</h3>")
+            return HttpResponse("<h3>Введіть прізвище</h3>")
         elif email is None:
-            return HttpResponse("<h3>Введите почту</h3>")
+            return HttpResponse("<h3>Введіть пошту</h3>")
         elif password is None:
-            return HttpResponse("<h3>Введите пароль</h3>")
+            return HttpResponse("<h3>Введіть пароль</h3>")
         elif date_of_birth is None:
-            return HttpResponse("<h3>Введите дату рождения</h3>")
+            return HttpResponse("<h3>Введіть дату народження</h3>")
         else:
-            user = NewUser.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password, )
+
+            # Create a new user using the NewUser model
+            user = NewUser.objects.create_user(first_name=first_name, last_name=last_name, email=email,
+                                               password=password)
+
+            # Log in the user after successful registration
             login(request, user)
+
+            # Redirect to the index page
             return redirect('index')
+
+    # If the request method is not POST, render the signup.html template
     return render(request, 'signup.html')
 
 
@@ -67,3 +114,19 @@ def logout_view(request):
 def change_user(request):
     logout(request)
     return redirect('login_view')
+
+
+
+# @login_required
+# def add_note(request):
+#     if request.method == 'POST':
+#         note_text = request.POST['note-text']
+#         if note_text:
+#             Note.objects.create(user=request.user, text=note_text)
+#             return redirect('add_note')  # Возвращаемся на страницу добавления заметки
+#     return render(request, 'add_note.html')  # Показываем страницу добавления заметки
+#
+# @login_required
+# def notes(request):
+#     user_notes = Note.objects.filter(user=request.user)
+#     return render(request, 'notes.html', {'user_notes': user_notes})
