df7afd726ce1cab6d4a2b75f80b6b24d97cf9b86
heorh-hlv <138703607+heorh-hlv@users.noreply.github.com>
2024-06-07 11:00:24 +0200

    Added requirements.txt

--- Code/Diploma_UTicket/UTicket/urls.py
+++ Code/Diploma_UTicket/UTicket/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path("", views.home_page, name="index"),
     path("index", views.home_page, name="index_snd"),
-    path("login", views.login_view, name="login_view"), #auth_views.LoginView.as_view(template_name='login.html')
+    path("login", views.login_view, name="login_view"),
     path("logout", views.logout_view, name="logout_view"),
     path("change_user", views.change_user, name="change_user"),
     path("signup", views.signup, name="signup"),
